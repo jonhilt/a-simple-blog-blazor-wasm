ac6f1a55f06a7f7f35e0721792dc61d36663b818
Jon Hilton <hilton.jon@gmail.com>
2020-06-26 13:27:17 +0100

    added anonymous version of the HTTPClient for making unauthed requests to "public" resources

--- Client/Program.cs
+++ Client/Program.cs
@@ -18,11 +18,16 @@ namespace ASimpleBlogStarter.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddHttpClient("ASimpleBlogStarter.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            builder.Services.AddHttpClient("ASimpleBlogStarter.AnonymousAPI",
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+            builder.Services.AddHttpClient("ASimpleBlogStarter.ServerAPI",
+                    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ASimpleBlogStarter.ServerAPI"));
+            builder.Services.AddTransient(sp =>
+                sp.GetRequiredService<IHttpClientFactory>().CreateClient("ASimpleBlogStarter.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
 
